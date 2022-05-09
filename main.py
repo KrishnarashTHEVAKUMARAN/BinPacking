aec48253a3a67550c6d2ad0074b2eceb8ee4d8fb
Krishnarash <thevakumarankrishnarash@gmail.com>
2022-05-09 11:49:05 +0200

    Creation Projet Bin Packing

---
+++ main.py
@@ -0,0 +1,11 @@
+# Codé par THEVAKUMARAN Krishnarash, LASNAMI Sara
+# Optimisation Combinatoire
+# Bin Packing (Methode First Fit)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
